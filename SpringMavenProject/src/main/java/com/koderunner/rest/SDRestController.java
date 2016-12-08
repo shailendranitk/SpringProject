42e32d75510b3196f30ba04f11d4db2f91377a88
shailendranitk <shailendra.nitk@gmail.com>
2016-12-09 00:34:38 +0530

    Updating rest path

--- SpringMavenProject/src/main/java/com/koderunner/rest/SDRestController.java
+++ SpringMavenProject/src/main/java/com/koderunner/rest/SDRestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.koderunner.dao.SDUserDAO;
 
 @RestController
-@RequestMapping("/rest")
+@RequestMapping("/SpringMavenProject/rest")
 public class SDRestController {
 
     
