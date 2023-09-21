b9bff8e8e765df46e3c6b760f9f7c7d037c2bca3
shailendranitk <shailendra.nitk@gmail.com>
2023-09-21 22:44:06 +0530

    Updated index.js

--- ReactProject/employee-management/src/index.js
+++ ReactProject/employee-management/src/index.js
@@ -17,3 +17,78 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+/*
+console.log("Hello")
+
+
+const arr = [5,3,7,9];
+
+console.log(...arr);
+
+const person ={
+  name: "Shailendra",
+  age:"28",
+  phone:"923434423",
+  address:{
+    city:"New York",
+    country:"US"
+  }
+}
+
+const {address:{city}} = person
+
+console.log(person.name)
+
+console.log(city)
+
+function double(x){
+  return x*2;
+}
+
+console.log(arr.map((x)=> x*3 ))
+
+*/
+
+/*
+
+let myfunction =  () =>{
+  console.log("Hello world")
+}
+
+myfunction();
+
+*/
+
+
+/*
+
+
+class Employee{
+  constructor(name,age){
+    this.name = name;
+    this.age = age;
+  }
+
+  getEmployeeName(){
+    console.log(this.name, this.age);
+  }
+
+}
+
+const emp1 = new Employee("Shailendra",30);
+
+emp1.getEmployeeName();
+*/
+
+/*
+const arr = [1,2,3,5];
+
+let [x,y,z] = arr;
+
+console.log(x,y,z);
+
+
+*/
+
+
